495d72fa7ea38e46597184f5b2356aa2ad1660c5
myunghoonju <myunghoonju@gmail.com>
2021-10-25 11:58:37 +0900

    테스트 순서 지정하기

--- src/test/java/com/practice/tdd/junittest/NameTest.java
+++ src/test/java/com/practice/tdd/junittest/NameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // 전체 적용
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // 클래스마다 인스턴스 생성, 즉 아래 테스트는 하나의 클래스 인스턴스를 공유한다.
 class NameTest {
@@ -14,6 +15,7 @@ class NameTest {
         System.out.println("beforeAll");
     }
 
+    @Order(1)
     @Test
     void create_name() {
         Name name = new Name();
@@ -22,6 +24,7 @@ class NameTest {
         assertEquals(NameStatus.DRAFT, name.getStatus(), "default:: DRAFT");
     }
 
+    @Order(0)
     @Test
     void create_name_test() {
         Name name = new Name();
@@ -34,6 +37,7 @@ class NameTest {
         //assertEquals(NameStatus.DRAFT, name.getStatus(), "default:: DRAFT");
     }
 
+    @Order(2)
     @Test
     @DisplayName("테스트 이름 짓기")
     void create() {
