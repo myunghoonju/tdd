938d1bbd60730dc6cf5eb9dc93e8a3bfbab05511
myunghoonju <myunghoonju@gmail.com>
2021-11-16 14:47:32 +0900

    TEST

--- src/main/java/com/practice/tdd/mock/study/StudyService.java
+++ src/main/java/com/practice/tdd/mock/study/StudyService.java
@@ -16,8 +16,6 @@ public class StudyService {
     private final StudyRepository studyRepository;
 
     public StudyService(MemberService memberService, StudyRepository studyRepository) {
-        assert memberService != null;
-        assert studyRepository != null;
         this.memberService = memberService;
         this.studyRepository = studyRepository;
     }
--- src/test/java/com/practice/tdd/mock/study/StudyServiceTest.java
+++ src/test/java/com/practice/tdd/mock/study/StudyServiceTest.java
@@ -1,12 +1,17 @@
 package com.practice.tdd.mock.study;
 
+import com.practice.tdd.mock.domain.Member;
+import com.practice.tdd.mock.domain.Study;
 import com.practice.tdd.mock.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -18,9 +23,59 @@ class StudyServiceTest {
     StudyRepository studyRepository;
 
     @Test
-    void createStudyService() {
+    void createStudyOne() {
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
+
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setEmail("myunghoonju@gmail.com");
+
+        //when -> a method has a return type.
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+        Study study = new Study(10, "java");
+        Optional<Member> member1 = memberService.findById(1L); // 2, 3, 4... etc..,
+        assertEquals("myunghoonju@gmail.com", member1.get().getEmail());
+
+        studyService.createNewStudy(1L, study);
+
+        //exception test
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+
+
+    }
+
+    @Test
+    void createStudyTwo() {
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setEmail("myunghoonju@gmail.com");
+
+        //multiple call
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        //.thenReturn(Optional.of(member))
+        Optional<Member> member2 = memberService.findById(1L);
+        assertEquals("myunghoonju@gmail.com", member2.get().getEmail());
+
+        //.thenThrow(new RuntimeException())
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(1L);
+        });
+
+        //.thenReturn(Optional.empty());
+        assertEquals(Optional.empty(), memberService.findById(1L));
     }
 
 }
